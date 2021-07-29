934896cfbf9ac12f583b58c89113839d6693b54b
HennessyZazzz <60851577+HennessyZazzz@users.noreply.github.com>
2021-07-29 15:03:59 +0300

    Add help page and fix bags

--- Targetcom/Targetcom/wwwroot/js/site.js
+++ Targetcom/Targetcom/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
