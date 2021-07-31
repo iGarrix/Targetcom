7a57ded674e0d94c4dffe6e1a1fd7ab6fde45b82
Tarn_6txg@student.itstep.org <Tarn_6txg@student.itstep.org>
2021-07-31 12:23:29 +0300

    Create newsVM

---
+++ Targetcom/Targetcom/Models/NewsVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Targetcom.Models
+{
+    public class NewsVM
+    {
+        public Profile IdentityProfile { get; set; }
+    }
+}
