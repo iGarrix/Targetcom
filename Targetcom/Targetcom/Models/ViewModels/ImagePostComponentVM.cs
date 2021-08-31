03782475eb41cd696c2059ed0e73eed218ed8cab
Tarn_6txg@student.itstep.org <Tarn_6txg@student.itstep.org>
2021-08-31 23:02:50 +0300

    Save Changes

---
+++ Targetcom/Targetcom/Models/ViewModels/ImagePostComponentVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Targetcom.Models.ViewModels
+{
+    public class ImagePostComponentVM
+    {
+        public Profile IdentityProfile { get; set; }
+        public ProfilePostage ImageModel { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
